1a2671b50f46c56cc7371df2ed92c384a1cd44fc
Aakash Nishad <111107009+aksxil@users.noreply.github.com>
2024-12-03 12:54:14 +0530

    Update db.js

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -6,7 +6,6 @@ dotenv.config();
 export const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGODB_URI, {
-      useNewUrlParser: true, // Still recommended for parsing the connection string
       connectTimeoutMS: 90000, // Set connection timeout to 30 seconds
       socketTimeoutMS: 45000, // Set socket timeout to 45 seconds
     });
