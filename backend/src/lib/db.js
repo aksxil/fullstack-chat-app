ee626c970f68549cbefb6977f23179d5afc9785e
Aakash Nishad <111107009+aksxil@users.noreply.github.com>
2024-12-03 12:45:12 +0530

    Update db.js

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -1,16 +1,19 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 export const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGODB_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
-      connectTimeoutMS: 30000, // Set timeout to 30 seconds
-      socketTimeoutMS: 45000, // Set socket timeout to 45 seconds
+      connectTimeoutMS: 500000,
+      socketTimeoutMS: 45000,
     });
     console.log(`MongoDB connected: ${conn.connection.host}`);
   } catch (error) {
-    console.error("MongoDB connection error:", error);
-    process.exit(1); // Exit the process with failure code
+    console.error("MongoDB connection error:", error.message);
+    process.exit(1); // Exit the application on connection failure
   }
 };
